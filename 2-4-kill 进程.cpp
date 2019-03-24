4e4d0a0841a1ef29efcd8af76bb1e1d634fbe9af
cs <41311403+bjtucs@users.noreply.github.com>
2019-03-24 18:38:47 +0800

    操作系统第二次实验

---
+++ 2-4-kill 进程.cpp
@@ -0,0 +1,28 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main(){
+pid_t pid;
+pid = fork();
+if (pid > 0){
+fork();
+while(1){
+printf ("%d I am the parent of pid=%d!\n",getpid(),getppid());
+sleep(10);
+}
+}
+else if (!pid){
+pid = fork();
+if(pid > 0)
+fork();
+while(1){
+printf ("%d I am the parent of pid=%d!\n",getpid(),getppid());
+sleep(10);
+}
+}
+else if (pid == -1)
+perror (¡°fork¡±);
+return 0;
+}
+
